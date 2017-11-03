973e76b1a6f6196f4cd15c88cedf537ca2101280
Kyle Krawl <kyle.krawl@gmail.com>
2017-11-03 14:52:56 -0600

    Implemented ability to add and remove players from user team.

--- app/nfl-roster/nfl-roster-controller.js
+++ app/nfl-roster/nfl-roster-controller.js
@@ -3,10 +3,12 @@ var PlayersController = function () {
     var loading = true; //Start the spinner
     var playersService = new PlayersService(ready);
     
-    function ready(){
+    function ready(service){
         loading = false; //stop the spinner
-    
         //Now that all of our player data is back we can safely setup our bindings for the rest of the view.
+        console.log('players service: ', service)
+
+        updateAvailablePlayers(service.getAvailablePlayers())
         
         /*
         $('some-button').on('click',function(){
@@ -14,8 +16,66 @@ var PlayersController = function () {
         }
         /*/
 
-
     }
 
+    this.add = function add(id) {
+        playersService.addToUserTeam(id)
+        updateAvailablePlayers(playersService.getAvailablePlayers())
+        updateUserTeam(playersService.getUserTeam())
+        console.log('all players: ', playersService.getAvailablePlayers())
+        console.log('user team: ', playersService.getUserTeam())
+      }
+    
+      this.remove = function remove(id) {
+        playersService.removeFromUserTeam(id)
+        updateAvailablePlayers(playersService.getAvailablePlayers())
+        updateUserTeam(playersService.getUserTeam())
+        console.log('all players: ', playersService.getAvailablePlayers())
+        console.log('user team: ', playersService.getUserTeam())
+      }
 
+    // fxn update available players display
+
+   function updateAvailablePlayers(list) {
+        var elem = document.getElementById('available-players')
+        elem.innerHTML = ''
+        var template = ''
+        for (var i in list) {
+          var player = list[i];
+          template += `
+          <div class="col-sm-3 text-center">
+            <img src="${player.imagePath}">
+            <h3>${player.firstName} ${player.lastName}</h3>
+              <div>
+                <button class="btn-success" id="${player.id}" onclick="app.controllers.playersController.add('${player.id}')">Add to Team</button>
+              </div>
+          </div>
+          `
+          elem.innerHTML = template
+        }
+    
+      }
+
+    // fxn update player roster
+
+    function updateUserTeam(list) {
+        var elem = document.getElementById('user-team')
+        elem.innerHTML = ''
+        var template = ''
+        for (var i in list) {
+          var player = list[i];
+          template += `
+          <div class="col-sm-3 text-center">
+            <img src="${player.imagePath}">
+            <h3>${player.firstName} ${player.lastName}</h3>
+              <div>
+                <button class="btn-danger" id="${player.id}" onclick="app.controllers.playersController.remove('${player.id}')">Remove from Team</button>
+              </div>
+          </div>
+          `
+          elem.innerHTML = template
+        }
+    
+      }
+      
 }
--- app/nfl-roster/nfl-roster-service.js
+++ app/nfl-roster/nfl-roster-service.js
@@ -1,18 +1,81 @@
 var PlayersService = function (callback) {
-    var endpointUri = "http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
     var playersData = [];
+    var userTeam = [];
+    var service = this
 
+    this.getAvailablePlayers = function getAvailablePlayers() {
+        return JSON.parse(JSON.stringify(playersData))
+    }
 
-    this.getPlayersByTeam = function (teamName) {
-        return playersData.filter(function(player) {
-            if (player.team == teamName) {
+    this.getUserTeam = function getUserTeam() {
+        return JSON.parse(JSON.stringify(userTeam))
+    }
+
+    this.getTeams = function getTeams() {
+        var out = []
+        for (i in playersData) {
+            var player = playersData[i]
+            if (!out.includes(player.teamName)) {
+                out.push(player.teamName)
+            }
+        }
+        return JSON.parse(JSON.stringify(out))
+    }
+
+    this.getPlayersByTeam = function getPlayersByTeam(teamName) {
+        return playersData.filter(function (player) {
+            if (player.teamName === teamName) {
                 return true;
             }
         });
     }
 
-    this.getPlayersByPosition = function (position) {
-        // ...
+    this.getPlayersByPosition = function getPlayersByPosition(position) {
+        return playersData.filter(function (player) {
+            if (player.position === position) {
+                return true;
+            }
+        });
+    }
+
+    this.getPlayersByFirstName = function getPlayersByFirstName(firstName) {
+        return playersData.filter(function (player) {
+            if (player.firstName === firstName) {
+                return true;
+            }
+        });
+    }
+
+    this.getPlayersByLastName = function getPlayersByLastName(lastName) {
+        return playersData.filter(function (player) {
+            if (player.lastName === lastName) {
+                return true;
+            }
+        });
+    }
+
+    this.addToUserTeam = function (id) {
+        for (i in playersData) {
+            var player = playersData[i]
+            if (player.id === id && !userTeam.includes(player)) {
+                userTeam.push(player)
+                playersData.splice(playersData.indexOf(player), 1)
+                break
+            }
+        }
+        console.log(userTeam)
+    }
+
+    this.removeFromUserTeam = function (id) {
+        for (i in userTeam) {
+            var player = userTeam[i]
+            if (player.id === id) {
+                playersData.push(player)
+                userTeam.splice(userTeam.indexOf(player), 1)
+                break
+            }
+        }
+        console.log(userTeam)
     }
 
     function loadPlayersData() {
@@ -24,8 +87,10 @@ var PlayersService = function (callback) {
         var localData = localStorage.getItem('playersData');
         if (localData) {
             playersData = JSON.parse(localData);
-            console.log(playersData)
-            return callback();
+            console.log('player data: ', playersData)
+            return callback(service) 
+            // Have to pass service instance to callback or service will 
+            // be inaccessible when loading from localStorage (not yet instantiated)
             //return will short-circuit the loadPlayersData function
             //this will prevent the code below from ever executing
         }
@@ -35,12 +100,25 @@ var PlayersService = function (callback) {
         var apiUrl = url + encodeURIComponent(endpointUri);
 
         $.getJSON(apiUrl, function (data) {
-            playersData = data.body.players;
+            //playersData = data.body.players
+
+            playersData = data.body.players.slice(0, 30).map(function (player) {
+                return {
+                    firstName: player.firstname,
+                    lastName: player.lastname,
+                    teamName: player.pro_team,
+                    position: player.position,
+                    imagePath: player.photo,
+                    id: player.id
+                }
+            });
+
             console.log('Player Data Ready')
             console.log('Writing Player Data to localStorage')
             localStorage.setItem('playersData', JSON.stringify(playersData))
             console.log('Finished Writing Player Data to localStorage')
-            callback()
+            console.log('player data: ', playersData)
+            callback(service)
         });
     }
     loadPlayersData(); //call the function above every time we create a new service
